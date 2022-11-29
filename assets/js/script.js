b5cbd6d3f23857d57a59d6eb0f6ce0e69aba107b
Justin Yi <justinjyi96@gmail.com>
2022-11-28 20:20:32 -0500

    creating function to prompt users for their choice of characters the password should contain

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,52 @@ var uppCaseChar = ("A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z").split(
 var numberChar = ("1,2,3,4,5,6,7,8,9,0").split(',');
 var specChar = ("~,!,@,#,$,%,^,&,*,(,),-,_,=,+,/,?,;,:").split(',');
 
+// Function to prompt user for which type of characters they want in their generated password
+// Fail-safe to ensure that desired password length is atleast 8 characters and no more than 128 characters
+// Fail-safe to ensure that input for character length is a number/converted to a number
+// Fail-safe to ensure that atleast one type of character is selected
+
+function userChoices() {
+  var passLength = parseInt(prompt("How long in characters would you like your password to be?"));
+  
+  if (Number.isNaN(passLength)) {
+    alert("Please enter a number to indicate how long the password should be");
+  };
+
+  if (passLength < 8) {
+    alert("Password must be atleast 8 characters in length.");
+  };
+
+  if (passLength > 128) {
+    alert("Password cannot be more than 128 characters in length");
+  };
+
+  var lowCaseOption = confirm("Would you like your password to contain lower case letters?");
+  var uppCaseOption = confirm("Would you like your password to contain upper case letters?");
+  var numberCharOption = confirm("Would you like your passowrd to contain numbers?");
+  var specCharOption = confirm("Would you like your password to contain special characters?");
+}
+
+
+userChoices();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
