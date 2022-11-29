34be22ecb88439210898c13f18f374d2fcd74305
Justin Yi <justinjyi96@gmail.com>
2022-11-28 21:39:52 -0500

    writing function to shuffle select arrays when chosen by user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ var specChar = ("~,!,@,#,$,%,^,&,*,(,),-,_,=,+,/,?,;,:").split(',');
 // Fail-safe to ensure that input for character length is a number/converted to a number
 // Fail-safe to ensure that atleast one type of character is selected
 
-function userChoices() {
+function passCriteria() {
   var passLength = parseInt(prompt("How long in characters would you like your password to be?"));
   
   if (Number.isNaN(passLength)) {
@@ -27,45 +27,97 @@ function userChoices() {
 
   var lowCaseOption = confirm("Would you like your password to contain lower case letters?");
   var uppCaseOption = confirm("Would you like your password to contain upper case letters?");
-  var numberCharOption = confirm("Would you like your passowrd to contain numbers?");
+  var numberOption = confirm("Would you like your passowrd to contain numbers?");
   var specCharOption = confirm("Would you like your password to contain special characters?");
+
+  if (
+  lowCaseOption === false && 
+  uppCaseOption === false && 
+  numberOption === false && 
+  specCharOption === false ) {
+  alert("Password must contain atleast one type of character!");
+  };
+
+
+  var passDetails = {
+    passLength: passLength,
+    lowCaseOption: lowCaseOption,
+    uppCaseOption: uppCaseOption,
+    numberOption: numberOption,
+    specCharOption: specCharOption,
+  };
+
+
+
+  function shufflearray(arraytoshuffle) {
+    for (i = arraytoshuffle.length-1; i>0; i--) {
+      var randPos = Math.floor(Math.random() * (i+1));
+      var temp = arraytoshuffle[i];
+
+      arraytoshuffle[i] = arraytoshuffle[randPos];
+      arraytoshuffle[randPos] = temp;
+    }
+    return arraytoshuffle;
+  }
+
+  if (passDetails.lowCaseOption) {
+    var randLowCaseChar = shufflearray(lowCaseChar);
+    console.log(randLowCaseChar);
+  };
+  
+  if (passDetails.uppCaseOption) {
+    var randUppCaseChar = shufflearray(uppCaseChar);
+    console.log(randUppCaseChar);
+  };
+
+  if (passDetails.numberOption) {
+    var randNumberChar = shufflearray(numberChar);
+    console.log(randNumberChar);
+  };
+
+  if (passDetails.specCharOption) {
+    var randSpecCaseChar = shufflearray(specChar);
+    console.log(randSpecCaseChar);
+  };
+
+  
 }
 
-
-userChoices();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", passCriteria);
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
