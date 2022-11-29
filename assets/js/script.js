eba8ab88bf64f0d88ea3f7f6ebba15d7fb5bd46d
Justin Yi <justinjyi96@gmail.com>
2022-11-28 19:44:29 -0500

    creating variables/array to contain all unique characters to be used in generated password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+// Define variables for password
+
+var lowCaseChar = ("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z").split(',');
+var uppCaseChar = ("A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z").split(',');
+var numberChar = ("1,2,3,4,5,6,7,8,9,0").split(',');
+var specChar = ("~,!,@,#,$,%,^,&,*,(,),-,_,=,+,/,?,;,:").split(',');
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
